3cf696881f0b8ac62903f904cce0461ce537280b
olivercastroteran <olivercastroteran@gmail.com>
2020-06-02 20:13:18 -0400

    beatmaker styled

---
+++ beatmaker/app.js
@@ -0,0 +1,8 @@
+window.addEventListener('load', () => {
+  const sounds = document.querySelectorAll('.sound');
+  const pads = document.querySelectorAll('.pads div');
+
+  pads.forEach((pad, i) => {
+    pad.addEventListener('click', function () {});
+  });
+});
