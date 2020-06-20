6c46f65813e3691b16a88a8a01c197db7558880b
olivercastroteran <olivercastroteran@gmail.com>
2020-06-19 21:40:30 -0400

    infinite scroll - infinite scroll and filter implemented

--- infinite-scroll/app.js
+++ infinite-scroll/app.js
@@ -3,7 +3,7 @@ const postContainer = document.getElementById('post-container');
 const loading = document.querySelector('.loader');
 const filter = document.getElementById('filter');
 
-let limit = 3;
+let limit = 5;
 let page = 1;
 
 // fetch posts from API
@@ -39,5 +39,46 @@ async function showPosts() {
   });
 }
 
+// Show loader and fetch more posts
+function showLoading() {
+  loading.classList.add('show');
+  setTimeout(() => {
+    loading.classList.remove('show');
+
+    setTimeout(() => {
+      page++;
+      console.log(page);
+      showPosts();
+    }, 300);
+  }, 1000);
+}
+
+// Filter posts by input
+function filterPosts(e) {
+  const term = e.target.value.toUpperCase();
+  const posts = document.querySelectorAll('.post');
+
+  posts.forEach((post) => {
+    const title = post.querySelector('.post-title').innerText.toUpperCase();
+    const body = post.querySelector('.post-body').innerText.toUpperCase();
+
+    if (title.indexOf(term) > -1 || body.indexOf(term) > -1) {
+      post.style.display = 'flex';
+    } else {
+      post.style.display = 'none';
+    }
+  });
+}
+
 // Show initial posts
 showPosts();
+
+window.addEventListener('scroll', () => {
+  const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+  if (scrollTop + clientHeight >= scrollHeight - 5) {
+    showLoading();
+  }
+});
+
+filter.addEventListener('input', filterPosts);
