2c2fbd68d13cac28ccf88812b4d337f7c7f3f279
olivercastroteran <olivercastroteran@gmail.com>
2020-06-28 10:49:44 -0400

    lyrics search app - rendering search result to the DOM

---
+++ lyrics-search-app/app.js
@@ -0,0 +1,106 @@
+// DOM elements
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const result = document.getElementById('result');
+const more = document.getElementById('more');
+
+const apiURL = 'https://api.lyrics.ovh';
+
+// Search by song or artist
+async function searchSongs(term) {
+  // fetch(`${apiURL}/suggest/${term}`)
+  //   .then((res) => res.json())
+  //   .then((data) => console.log(data));
+
+  const res = await fetch(`${apiURL}/suggest/${term}`);
+  const data = await res.json();
+
+  showData(data);
+}
+
+// Show song and artis in DOM
+function showData(data) {
+  //let output = '';
+
+  // data.data.forEach((song) => {
+  //   output += `
+  //     <li>
+  //       <span>
+  //         <strong>${song.artist.name}</strong> - ${song.title}
+  //       </span>
+  //       <button
+  //         class="btn"
+  //         data-artist="${song.artist.name}"
+  //         data-songtitle="${song.title}">
+  //           Get Lyrics
+  //       </button>
+  //     </li>
+  //   `;
+  // });
+
+  // result.innerHTML = `
+  //   <ul class="songs">
+  //     ${output}
+  //   </ul>
+  // `;
+
+  result.innerHTML = `
+    <ul class="songs">
+      ${data.data
+        .map((song) => {
+          return `
+        <li>
+          <span>
+            <strong>${song.artist.name}</strong> - ${song.title}
+          </span>
+          <button 
+            class="btn" 
+            data-artist="${song.artist.name}"
+            data-songtitle="${song.title}">
+              Get Lyrics
+          </button>
+        </li>
+      `;
+        })
+        .join('')}
+    </ul>
+  `;
+
+  if (data.prev || data.next) {
+    more.innerHTML = `
+      ${
+        data.prev
+          ? `<button class="btn" onclick="getMoreSongs('${data.prev}')">Prev</button>`
+          : ''
+      }
+      ${
+        data.next
+          ? `<button class="btn" onclick="getMoreSongs('${data.next}')">Next</button>`
+          : ''
+      }
+    `;
+  } else {
+    more.innerHTML = '';
+  }
+}
+
+// Get more songs prev and next
+async function getMoreSongs(url) {
+  const res = await fetch(`https://cors-anywhere.herokuapp.com/${url}`);
+  const data = await res.json();
+
+  showData(data);
+}
+
+// Event listeners
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const searchTerm = search.value.trim();
+
+  if (!searchTerm) {
+    alert('please type in a search term');
+  } else {
+    searchSongs(searchTerm);
+  }
+});
