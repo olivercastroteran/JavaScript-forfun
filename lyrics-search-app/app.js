1fca08d84307ee1424d387c2a0736363ec34d631
olivercastroteran <olivercastroteran@gmail.com>
2020-06-28 11:09:06 -0400

    lyrics search app - rendering lyrics to the DOM

--- lyrics-search-app/app.js
+++ lyrics-search-app/app.js
@@ -92,6 +92,21 @@ async function getMoreSongs(url) {
   showData(data);
 }
 
+// Get song lyrics
+async function getLyrics(artist, songTitle) {
+  const res = await fetch(`${apiURL}/v1/${artist}/${songTitle}`);
+  const data = await res.json();
+
+  const lyrics = data.lyrics.replace(/(\r\n|\r|\n)/g, '<br>');
+
+  result.innerHTML = `
+    <h2><strong>${artist}</strong> - ${songTitle}</h2>
+    <span>${lyrics}</span>
+  `;
+
+  more.innerHTML = '';
+}
+
 // Event listeners
 form.addEventListener('submit', (e) => {
   e.preventDefault();
@@ -102,5 +117,17 @@ form.addEventListener('submit', (e) => {
     alert('please type in a search term');
   } else {
     searchSongs(searchTerm);
+  }
+});
+
+// Get lyrics btn click
+result.addEventListener('click', (e) => {
+  const clickedEl = e.target;
+
+  if (clickedEl.tagName === 'BUTTON') {
+    const artist = clickedEl.getAttribute('data-artist');
+    const songTitle = clickedEl.getAttribute('data-songtitle');
+
+    getLyrics(artist, songTitle);
   }
 });
